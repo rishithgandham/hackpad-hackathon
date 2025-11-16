f389fe3b9973481ccc24c8bd4c0fe0d6f096a8a7
Rishith Gandham <rishith.gandham@gmail.com>
2025-11-16 11:20:09 -0500

    edited better auth config

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,5 +1,6 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
+    trustedOrigins: ["http://localhost:3000", "https://hackpad-hackathon.vercel.app/"],
 })
 
 export const { signIn, signUp, useSession, signOut } = createAuthClient()
